dfc4c3bd5899106ce83a4db95b676007189eed5d
Wesley Oliveira <wesleyoliveira11@gmail.com>
2019-08-15 15:18:24 -0300

    adicionada uma rota 'add' para receber os valores do titulo q o usuario digitar e o conteudo. Adicionado a dependência do bodyParse para requisições do formulario

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
-
+const bodyParser = require('body-parser');
 //Config
+
     //template engine
     app.engine('handlebars',handlebars({defaultLayout:'main'}));
     app.set('view engine', 'handlebars');
-    
+
+    //body parser
+    app.use(bodyParser.urlencoded({extended:false}));
+    app.use(bodyParser.json());
+
     //Conexão com o banco de dados MySql
     const Sequelize = require('sequelize');
     const sequelize = new Sequelize('test','root','123456789',{
         host:"localhost",
         dialect:'mysql'
+    });
+
+//Rotas
+
+    app.get('/cad', function(req,res){
+        res.render('form');
+    });
+
+    app.post('/add', function(req,res){
+        var titulo =req.body.titulo;
+        var conteudo =req.body.conteudo;
+        res.send("Texto: "+titulo+" conteudo: "+req.body.conteudo);
     });
 
 app.listen(8081,function(){
